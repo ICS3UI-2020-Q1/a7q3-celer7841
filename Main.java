0acf8ef7d20f41aabd7b2e540dcd622ad84509c1
celer7841 <celer7841@wrdsb.ca>
2020-10-14 04:25:58 +0000

    finished

--- Main.java
+++ Main.java
@@ -9,7 +9,22 @@ public class Main {
    * @param args the command line arguments
    */
   public static void main(String[] args) {
-    // TODO code application logic here
-    
+    chaotic(7);
   }
+  
+  public static void chaotic (int lineNumber){
+
+    for (int i=0;i<lineNumber;i++){
+      int randomNumber = (int)(Math.random()*(5-1+1))+1;
+      for (int j=1;j<=randomNumber;j++){
+        System.out.print("*");
+      } 
+      System.out.println();
+    }
+
+  }
+
 }
+    
+  
+
